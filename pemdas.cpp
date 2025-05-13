e274e3c05cd8c00ab362d07043ed2e0200ae35af
errerew <erlina31acc@gmail.com>
2025-05-13 18:55:45 +0700

    enghitung luas dan keliling dari bangun datar

--- pemdas.cpp
+++ pemdas.cpp
@@ -8,3 +8,17 @@ class persegiPanjang{
     void inputData(bangunDatar &bd);
     void outputData(bangunDatar &bd);
 };
+
+class bangunDatar{
+    private:
+    float panjang;
+    float lebar;
+    float hitungLuas(){
+        return panjang * lebar;
+    };
+    float hitungKeliling(){
+        return 2 * panjang + lebar;
+    };
+
+    
+}
